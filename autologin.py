edb29fe51c2124026933c477f5fb45010247f488
Peter Hu <yesky93@sina.com>
2016-03-28 22:13:00 +0800

    login

--- autologin.py
+++ autologin.py
@@ -11,19 +11,19 @@ class autoLogin:
     def init(self):
         None
 
-    def requestLogin(self,host,referUrl,loginUrl,user,pwd):
+    def requestLogin(self,option):
         cookiejar=cookielib.CookieJar()
         urlopener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
         urllib2.install_opener(urlopener)
-        urlopener.addheaders.append(('Referer', referUrl))
+        urlopener.addheaders.append(('Referer', option.referUrl))
         urlopener.addheaders.append(('Accept-Language', 'zh-CN'))
-        urlopener.addheaders.append(('Host', host))
+        urlopener.addheaders.append(('Host', option.host))
         urlopener.addheaders.append(('User-Agent', 'Mozilla/5.0 (compatible; MISE 9.0; Windows NT 6.1); Trident/5.0'))
         urlopener.addheaders.append(('Connection', 'Keep-Alive'))
-        downloadVodeImage(imageUrl,urlopener)
-        vocde = readVCode()
+        #downloadVodeImage(imageUrl,urlopener)
+        #vocde = readVCode()
         requestData = {}
-        replyContent = urlopener.open(urllib2.Request(url, urllib.urlencode(requestData)))
+        replyContent = urlopener.open(urllib2.Request(option.loginUrl, urllib.urlencode(requestData)))
         result = replyContent.read(50000)
 
         if result.find('login.jsp') != -1:
@@ -33,13 +33,14 @@ class autoLogin:
 
 
     def downloadVCodeImage(self, imageUrl):
-        try:
-            toSaveFile=open(r'code.jpg', 'w')
-            toSaveFile.write(urlopener.open(urllib2.Request(imageUrl)).read()
-            toSaveFile.close()
-            return True
-        except:
-            return False
+        None
+        # try:
+        #     toSaveFile=open(r'code.jpg', 'w')
+        #     toSaveFile.write(urlopener.open(urllib2.Request(imageUrl)).read()
+        #     toSaveFile.close()
+        #     return True
+        # except:
+        #     return False
 
     def readConfig(self):
         with open('config.json','r') as configFile:
@@ -52,10 +53,13 @@ class autoLogin:
         config = self.readConfig()
         print config
         loginConfig = config['login']
-        url = loginConfig['url']
-        user = loginConfig['user']
-        pwd = loginConfig['pwd']
-    
+        # url = loginConfig['url']
+        # user = loginConfig['user']
+        # pwd = loginConfig['pwd']
+        # host = loginConfig['host']
+        # referUrl = loginConfig['referUrl']
+        self.requestLogin(loginConfig);
+
     def log(self):
         None
 
@@ -65,6 +69,3 @@ class autoLogin:
 if __name__=='__main__':
     autoLogin = autoLogin()
     autoLogin.login()
-              
-
-
